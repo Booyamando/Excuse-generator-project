6a9b6f9c16d441e0979cf13d252456b48f9ddf0e
Booyamando <filibs10@gmail.com>
2020-11-20 00:13:28 +0000

    first init

--- src/main.js
+++ src/main.js
@@ -6,5 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let who = ["itme", "item2", "item3", "item4", "itme5"];
+  let what = ["itme", "item2", "item3", "item4", "itme5"];
+  let when = ["itme", "item2", "item3", "item4", "itme5"];
+
+  let sentence = `${who[getRndInteger(0, who.length - 1)]} ${
+    what[getRndInteger(0, what.length - 1)]
+  } ${when[getRndInteger(0, when.length - 1)]}`;
+
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  let excuse = document.querySelector("#excuse");
+
+  excuse.innerHTML = sentence;
 };
